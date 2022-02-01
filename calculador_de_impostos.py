451d527d34455c5b153891f04a6e27ccc5244701
MarlonMartins <marlonmartinsa7x@outlook.com>
2022-02-01 01:24:58 -0300

    Template Method

--- calculador_de_impostos.py
+++ calculador_de_impostos.py
@@ -1,4 +1,4 @@
-from impostos import ICMS, ISS
+from impostos import ICMS, ICPP, IKCV, ISS
 
 
 class CalculadorDeImpostos(object):
@@ -10,11 +10,19 @@ class CalculadorDeImpostos(object):
 
 
 if __name__ == "__main__":
-    from orcamento import Orcamento
+    from orcamento import Item, Orcamento
 
     calculador = CalculadorDeImpostos()
 
-    orcamento = Orcamento(500)
+    orcamento = Orcamento()
+    orcamento.adiciona_item(Item("ITEM - 1", 50))
+    orcamento.adiciona_item(Item("ITEM - 2", 200))
+    orcamento.adiciona_item(Item("ITEM - 3", 250))
 
+    print("ISS e ICMS")
     calculador.realiza_calculo(orcamento, ISS())
     calculador.realiza_calculo(orcamento, ICMS())
+
+    print("ICPP e IKVC")
+    calculador.realiza_calculo(orcamento, ICPP())
+    calculador.realiza_calculo(orcamento, IKCV())
